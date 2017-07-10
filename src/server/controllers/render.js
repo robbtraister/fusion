4d45cbdee0b3b3ad71eb1cf8ed23c086e9f0980a
Robb Traister <robb.traister@gmail.com>
2017-07-10 10:17:06 -0400

    use rendering object

--- src/server/controllers/render.js
+++ src/server/controllers/render.js
@@ -7,99 +7,109 @@ const debug = require('debug')(`fusion:controllers:render:${process.pid}`)
 const ReactDOMServer = require('react-dom/server')
 const request = require('request-promise-native')
 
-const wrapper = require('./wrapper')
-
 const Content = require('./content')
 const Templates = require('./templates')
+const wrapper = require('./wrapper')
 
-function render (templateName, contentURI, options, component, props, fetch, cache) {
+function Rendering (uri, options) {
+  if (!(this instanceof Rendering)) {
+    return new Rendering(uri, options)
+  }
+
+  this.options = options || {}
+  this.options.hydrated = false
+
+  this.contentURI = Content.resolve(uri)
+  this.templateName = Templates.resolve(uri)
+  this.component = Templates.load(this.templateName)
+}
+
+Rendering.prototype.render = function () {
+  return render(this)
+}
+
+Rendering.prototype.hydrate = function () {
+  this.options.hydrated = true
+
+  return Content.fetch(this.contentURI)
+    .then(JSON.parse.bind(JSON))
+    .then(content => {
+      this.content = content
+
+      this.cache = {}
+      this.fetch = (uri, component, asyncOnly) => {
+        if (!asyncOnly) {
+          debug('sync fetching', uri)
+
+          if (this.cache.hasOwnProperty(uri)) {
+            if (!(this.cache[uri] instanceof Promise)) {
+              return this.cache[uri]
+            }
+          } else {
+            // don't add global content to the cache unless a component requests it
+            this.cache[uri] = (
+              (uri === this.contentURI)
+              ? content
+              : request({
+                uri: `http://localhost:8080${uri}`,
+                json: true
+              })
+            ).then(json => { this.cache[uri] = json })
+          }
+        }
+        return null
+      }
+
+      // render with no cache; fetch will populate it as necessary
+      let dehydratedHTML = this.render()
+      let cacheKeys = Object.keys(this.cache)
+
+      let cachePromise = Promise.all(cacheKeys.map(k => this.cache[k]))
+
+      return {
+        // if getting content, we want it populated with the global content
+        // if rendering, we don't want to add global content to the html
+        // so don't add to the cache until we know which we are doing
+        // if a component requested global content uri, it will already be added
+        content: () => cachePromise
+          .then(() => { this.cache[this.contentURI] = content })
+          .then(() => this.cache),
+        // if no component content, don't re-render
+        render: () => cacheKeys.length
+          ? cachePromise.then(() => this.render())
+          : dehydratedHTML
+      }
+    })
+}
+
+function render (rendering) {
   return '<!DOCTYPE html>' +
     ReactDOMServer.renderToStaticMarkup(
-      wrapper(
-        templateName,
-        contentURI,
-        options,
-        component,
-        props,
-        fetch,
-        cache
-      )
+      wrapper(rendering)
     )
 }
 
-function contentCache (contentURI) {
-  let content = Content.fetch(contentURI)
-    .then(JSON.parse.bind(JSON))
-
-  let cache = {}
-  function fetch (uri, component, asyncOnly) {
-    if (!asyncOnly) {
-      debug('sync fetching', uri)
-
-      if (cache.hasOwnProperty(uri)) {
-        if (!(cache[uri] instanceof Promise)) {
-          return cache[uri]
-        }
-      } else {
-        // don't add global content to the cache unless a component requests it
-        cache[uri] = (
-          (uri === contentURI)
-          ? content
-          : request({
-            uri: `http://localhost:8080${uri}`,
-            json: true
-          })
-        ).then(json => { cache[uri] = json })
-      }
-    }
-    return null
-  }
-
-  return {
-    cache,
-    content,
-    fetch
-  }
-}
-
-function hydrate (templateName, contentURI, options) {
-  let { cache, content, fetch } = contentCache(contentURI)
-  let template = Templates.load(templateName)
-
-  return content.then(content => {
-    function renderHydrated (cache) {
-      return render(templateName, contentURI, options, template, content, fetch, cache)
-    }
-
-    // render with no cache; fetch will populate it as necessary
-    let dehydratedHTML = renderHydrated()
-    let cacheKeys = Object.keys(cache)
-
-    let cachePromise = Promise.all(cacheKeys.map(k => cache[k]))
-
-    return {
-      // if getting content, we want it populated with the global content
-      // if rendering, we don't want to add global content to the html
-      // so don't add to the cache until we know which we are doing
-      // if a component requested global content uri, it will already be added
-      content: () => cachePromise.then(() => { cache[contentURI] = content }).then(() => cache),
-      // if no component content, don't re-render
-      render: () => cacheKeys.length ? cachePromise.then(() => renderHydrated(cache)) : dehydratedHTML
-    }
-  })
-}
-
-function content (templateName, contentURI, options) {
-  return hydrate(templateName, contentURI, options)
+function content (uri, options) {
+  return new Rendering(uri, options)
+    .hydrate()
     .then(data => data.content())
 }
 
-function renderHydrated (templateName, contentURI, options) {
-  return hydrate(templateName, contentURI, options)
+function renderDehydrated (uri) {
+  return Promise.resolve(render(new Rendering(uri, { includeScripts: true })))
+}
+
+function renderHydrated (uri, options) {
+  return new Rendering(uri, options)
+    .hydrate()
     .then(data => data.render())
 }
 
 module.exports = renderHydrated
 module.exports.content = content
-module.exports.render = render
+module.exports.renderDehydrated = renderDehydrated
 module.exports.renderHydrated = renderHydrated
+
+if (module === require.main) {
+  renderDehydrated(process.argv[2] || '/').then(console.log)
+}

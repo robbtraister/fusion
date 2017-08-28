a5472896f8e155ac9c1d4744e990d891d266d60f
Robb Traister <robb.traister@gmail.com>
2017-08-28 16:47:57 -0400

    add router dev watcher

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@
 
 'use strict'
 
+const path = require('path')
+
 // const debug = require('debug')(`app:index:${process.pid}`)
 const express = require('express')
 const logger = require('winston')
@@ -12,6 +14,16 @@ const jsx = require('./engines/jsx')
 const vue = require('./engines/vue')
 
 function server (port) {
+  const router = /^prod/i.test(process.env.NODE_ENV)
+    ? require('./router')
+    : () => {
+      require('shell-watcher').pipe({target: path.resolve('.')})
+
+      return (req, res, next) => {
+        require('./router')()(req, res, next)
+      }
+    }
+
   const app = express()
 
   app.enable('trust proxy')
@@ -32,7 +44,7 @@ function server (port) {
 
   app.use(require('compression')())
 
-  app.use(require('./router')())
+  app.use(router())
 
   app.use((err, req, res, next) => {
     res.status(500).send(/^prod/i.test(process.env.NODE_ENV) ? '' : err)

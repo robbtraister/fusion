88218dbf0401d393c1c8058c3411bcffc3f6834b
Robb Traister <robb.traister@gmail.com>
2018-03-15 18:18:16 -0400

    make webpack configs available for other use

---
+++ engine/webpack-configs.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const path = require('path')
+
+const ManifestPlugin = require('webpack-manifest-plugin')
+
+const VENDOR_PACKAGES = ['react']
+
+function externals (context, request, callback) {
+  if (VENDOR_PACKAGES.includes(request)) {
+    return callback(null, request)
+  }
+  callback()
+}
+
+// if consumer is not found, attempt to build without it
+let alias = {}
+try {
+  alias = {
+    'consumer': require.resolve('./src/react/components/consumer.js')
+  }
+} catch (e) {}
+
+module.exports = (entry, outputPath) => {
+  return Object.keys(entry).length
+    ? {
+      entry,
+      externals,
+      mode: 'production',
+      module: {
+        rules: [
+          {
+            test: /\.jsx?$/i,
+            exclude: /node_modules/,
+            use: [
+              {
+                loader: 'babel-loader',
+                options: {
+                  babelrc: false,
+                  presets: [
+                    'es2015',
+                    'react'
+                  ],
+                  plugins: [
+                    'transform-decorators-legacy'
+                  ],
+                  comments: false
+                }
+              }
+            ]
+          }
+        ]
+      },
+      output: {
+        filename: `[name]`,
+        path: outputPath || path.resolve(__dirname, 'dist', 'components'),
+        libraryTarget: 'commonjs2'
+      },
+      plugins: [
+        new ManifestPlugin()
+      ],
+      resolve: {
+        alias,
+        extensions: ['.js', '.jsx']
+      }
+    }
+    : null
+}
--- engine/webpack.components.js
+++ engine/webpack.components.js
@@ -3,72 +3,11 @@
 const path = require('path')
 const glob = require('glob')
 
-const ManifestPlugin = require('webpack-manifest-plugin')
-
 const bundleRoot = process.env.BUNDLE_ROOT || `${__dirname}/bundle`
-
-const VENDOR_PACKAGES = ['react']
-
-function externals (context, request, callback) {
-  if (VENDOR_PACKAGES.includes(request)) {
-    return callback(null, request)
-  }
-  callback()
-}
-
-// if consumer is not found, attempt to build without it
-let alias = {}
-try {
-  alias = {
-    'consumer': require.resolve('./src/react/components/consumer.js')
-  }
-} catch (e) {}
-
 const componentDir = path.resolve(`${bundleRoot}/components`)
+
 const entry = {}
 glob.sync(`${componentDir}/**/*.{hbs,js,jsx,vue}`)
   .forEach(f => { entry[f.substr(componentDir.length + 1)] = f })
 
-module.exports = Object.keys(entry).length
-  ? {
-    entry,
-    externals,
-    mode: 'production',
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/i,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'babel-loader',
-              options: {
-                babelrc: false,
-                presets: [
-                  'es2015',
-                  'react'
-                ],
-                plugins: [
-                  'transform-decorators-legacy'
-                ],
-                comments: false
-              }
-            }
-          ]
-        }
-      ]
-    },
-    output: {
-      filename: `[name]`,
-      path: path.resolve(__dirname, 'dist', 'components'),
-      libraryTarget: 'commonjs2'
-    },
-    plugins: [
-      new ManifestPlugin()
-    ],
-    resolve: {
-      alias,
-      extensions: ['.js', '.jsx']
-    }
-  }
-  : null
+module.exports = require('./webpack-configs')(entry)

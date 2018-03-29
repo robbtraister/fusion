aaf6edce6b5a08e1478cb0851dd8e1f4acaeb6dc
Robb Traister <robb.traister@gmail.com>
2018-03-28 21:53:49 -0400

    support png file types

--- engine/src/index.js
+++ engine/src/index.js
@@ -1,15 +1,22 @@
 'use strict'
 
-const serverless = require('serverless-http')
-
 const app = require('./app')
 
-module.exports = {
-  app,
-  filter: require('./content/filter'),
-  render: require('./react/server/render'),
-  router: require('./router'),
-  schemas: require('./content/schemas'),
-  serverless: serverless(app),
-  sources: require('./content/sources')
+if (module === require.main) {
+  const port = process.env.PORT || 8080
+  app.listen(port, (err) => {
+    err ? console.error(err) : console.log(`Listening on port: ${port}`)
+  })
+} else {
+  const serverless = require('serverless-http')
+
+  module.exports = {
+    app,
+    filter: require('./content/filter'),
+    render: require('./react/server/render'),
+    router: require('./router'),
+    schemas: require('./content/schemas'),
+    serverless: serverless(app, {binary: ['image/png']}),
+    sources: require('./content/sources')
+  }
 }

70bfbb3defbf865c14a91a347b0954ee4c0f32aa
Robb Traister <robb.traister@gmail.com>
2018-04-04 16:41:24 -0400

    update package versions; go back to native aws-sdk to resolve permissions issue

--- engine/src/scripts/index.js
+++ engine/src/scripts/index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const S3 = require('aws-sdk').S3
 const zlib = require('zlib')
 
 const pack = require('../react/server/compile/pack')
@@ -8,7 +9,7 @@ const {
   findRenderableItem
 } = require('../renderings')
 
-const s3 = require('aws-promises').s3('us-east-1')
+const s3 = new S3({region: 'us-east-1'})
 
 const ENVIRONMENT = process.env.ENVIRONMENT
 const API_PREFIX = `/${process.env.CONTEXT || 'pb'}/api/v3`
@@ -51,20 +52,24 @@ const getScriptUrl = function getScriptUrl (pt) {
 }
 
 const uploadScript = function uploadScript (key, src) {
-  return new Promise((resolve, reject) => {
-    zlib.gzip(src, (err, buf) => {
-      err ? reject(err) : resolve(buf)
-    })
-  }).then((buf) => s3.upload(
-    getScriptBucket(),
-    key,
-    buf,
-    {
-      ACL: 'public-read',
-      ContentType: 'application/javascript',
-      ContentEncoding: 'gzip'
-    }
-  ))
+  return (UPLOAD_SCRIPTS)
+    ? new Promise((resolve, reject) => {
+      zlib.gzip(src, (err, buf) => {
+        err ? reject(err) : resolve(buf)
+      })
+    }).then((buf) => new Promise((resolve, reject) => {
+      s3.upload({
+        Bucket: getScriptBucket(),
+        Key: key,
+        Body: buf,
+        ACL: 'public-read',
+        ContentType: 'application/javascript',
+        ContentEncoding: 'gzip'
+      }, (err, data) => {
+        err ? reject(err) : resolve(data)
+      })
+    }))
+    : Promise.resolve()
 }
 
 const compile = function compile ({pt, rendering, child, isAdmin}) {
@@ -76,7 +81,7 @@ const compile = function compile ({pt, rendering, child, isAdmin}) {
     }
     : {
       rootRenderable: rendering,
-      upload: (UPLOAD_SCRIPTS && pt && !isAdmin)
+      upload: (pt && !isAdmin)
         ? (src) => uploadScript(`${getScriptPrefix()}${getScriptKey(pt)}`, src)
         // if in dev mode, do not upload script
         : () => Promise.resolve()

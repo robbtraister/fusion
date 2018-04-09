3e74102a4e2abc19455a3ebc36f4156cd75616e3
Robb Traister <robb.traister@gmail.com>
2018-04-09 11:01:51 -0400

    add localhost/docker-compose.data.yml for local db support

---
+++ localhost/data/run.sh
@@ -0,0 +1,67 @@
+#!/bin/sh
+
+restore() {
+  (
+    cd /data/restore
+    directories=$(ls -d */ 2> /dev/null)
+    if [ "$directories" ]
+    then
+      flush="true"
+      for directory in $directories
+      do
+        DB_NAME="${directory%%/}"
+        echo "restoring data into ${DB_NAME}..."
+        mongorestore -d "${DB_NAME}" --drop "./${DB_NAME}"
+        rm -rf "./${DB_NAME}"
+        echo "${DB_NAME} restored."
+      done
+    fi
+
+    tarballs=$(ls *.tar.gz 2> /dev/null)
+    if [ "$tarballs" ]
+    then
+      flush="true"
+      for tarball in $tarballs
+      do
+        DB_NAME="${tarball%%.tar.gz}"
+        echo "restoring data into ${DB_NAME}..."
+        rm -rf "./${DB_NAME}"
+        mkdir -p "./${DB_NAME}"
+        tar xf ${tarball} --strip-components 1 -C "./${DB_NAME}"
+        mongorestore -d "${DB_NAME}" --drop "./${DB_NAME}"
+        rm -rf "./${DB_NAME}"
+        rm -rf ${tarball}
+        echo "${DB_NAME} restored."
+      done
+    fi
+
+    if [ "$flush" ]
+    then
+      echo 'flushing cache...'
+      for p in `seq 7000 7007`
+      do
+        echo flushall | nc cache $p
+      done
+      echo flush_all | nc cache 11211
+      echo 'done.'
+    fi
+  )
+}
+
+watch() {
+  while [ true ]
+  do
+    restore
+    sleep ${SLEEP:-1}
+  done
+}
+
+mongod --fork --logpath /dev/stdout
+restore
+mongod --shutdown
+
+if [ "$WATCH" = 'true' ]
+then
+  watch &
+fi
+mongod
--- localhost/deploy.sh
+++ localhost/deploy.sh
@@ -2,4 +2,5 @@
 
 cd $(dirname "$0") && \
   docker-compose -f ./docker-compose.build.yml build && \
-  docker push quay.io/washpost/fusion-localhost
+  docker push quay.io/washpost/fusion-localhost && \
+  docker push quay.io/washpost/mongo-localhost

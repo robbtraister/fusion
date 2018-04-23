5c7d66d5828936130c279204b8046ec4fb802b23
Jonathan Satria <jonathan.satria@washpost.com>
2018-04-23 15:55:25 -0400

    Fix bad merge for make.js

--- resolver/src/controllers/make.js
+++ resolver/src/controllers/make.js
@@ -3,8 +3,9 @@
 const resolve = require('./resolve')
 const engine = require('./engine')
 
-const endpoint = function endpoint (data) {
-  return `/render/${data.type}`
+const endpoint = function endpoint (data, outputType) {
+  const query = outputType ? `?outputType=${outputType}` : ''
+  return `/render/${data.type}${query}`
 }
 
 const make = function make (uri, outputType, version) {

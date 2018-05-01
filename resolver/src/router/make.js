716de396f06538f9694533715e3a9474d9d148f5
Robb Traister <robb.traister@gmail.com>
2018-05-01 15:24:19 -0400

    add query param support; distinguish mock pages

--- resolver/src/router/make.js
+++ resolver/src/router/make.js
@@ -7,7 +7,8 @@ const make = require('../controllers/make')
 const makeRouter = express.Router()
 
 makeRouter.get('*', (req, res, next) => {
-  make(req.url, req.query.outputType, req.get('Fusion-Engine-Version'), req.get('Arc-Site'))
+  const arcSite = req.get('Arc-Site') || req.query._website
+  make(req.url, req.query.outputType, req.get('Fusion-Engine-Version'), arcSite)
     .then(data => { res.send(data) })
     .catch(next)
 })

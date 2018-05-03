4387d877f0aa4142baf3301c0e4d39b50f9a23f0
Robb Traister <robb.traister@gmail.com>
2018-05-03 09:06:59 -0400

    abstract trailing slash middleware

--- resolver/src/utils/trailing-slash-rule.js
+++ resolver/src/utils/trailing-slash-rule.js
@@ -22,24 +22,18 @@ const RedirectError = (statusCode, location) => {
   return e
 }
 
-const TRAILING_SLASH_REDIRECTS = {
-  DROP: (req, res, next) => {
-    const parts = url.parse(req.url)
-    TRAILING_SLASH_PATTERN.DROP.test(parts.pathname)
-      ? next(RedirectError(302, url.format(Object.assign(parts, {pathname: TRAILING_SLASH_REWRITES.DROP(parts.pathname)}))))
-      : next()
-  },
-  FORCE: (req, res, next) => {
-    const parts = url.parse(req.url)
-    TRAILING_SLASH_PATTERN.FORCE.test(parts.pathname)
-      ? next(RedirectError(302, url.format(Object.assign(parts, {pathname: TRAILING_SLASH_REWRITES.FORCE(parts.pathname)}))))
-      : next()
-  }
-}
+const trailingSlashRedirectMiddleware = (rule) =>
+  TRAILING_SLASH_PATTERN[rule]
+    ? null
+    : (req, res, next) => {
+      const parts = url.parse(req.url)
+      TRAILING_SLASH_PATTERN[rule].test(parts.pathname)
+        ? next(RedirectError(302, url.format(Object.assign(parts, {pathname: TRAILING_SLASH_REWRITES[rule](parts.pathname)}))))
+        : next()
+    }
 
 module.exports = {
-  TRAILING_SLASH_REDIRECTS,
   TRAILING_SLASH_REWRITES,
-  trailingSlashRedirect: TRAILING_SLASH_REDIRECTS[trailingSlashRule],
+  trailingSlashRedirect: trailingSlashRedirectMiddleware(trailingSlashRule),
   trailingSlashRewrite: TRAILING_SLASH_REWRITES[trailingSlashRule] || TRAILING_SLASH_REWRITES.NOOP
 }

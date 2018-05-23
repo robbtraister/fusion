aa184e687fdd509f7f154aa670db58f822ee1501
Robb Traister <robb.traister@gmail.com>
2018-05-23 09:34:38 -0400

    initial dynamoose DAO

--- engine/src/dao/dynamoose.js
+++ engine/src/dao/dynamoose.js
@@ -2,156 +2,50 @@
 
 const dynamoose = require('dynamoose')
 
+const {
+  environment,
+  region,
+  version
+} = require('../../environment')
+
 const defaultSchema = new dynamoose.Schema(
   {
-    env: {
+    version: {
       type: String,
       hashKey: true
     },
     id: {
       type: String,
-      default: model => uuidv4(),
       rangeKey: true
-    },
-    type: {
-      type: String,
-      required: true,
-      index: {
-        global: false
-      }
-    },
-    timestamp: {
-      type: Number,
-      required: true,
-      index: {
-        global: false
-      }
-    },
-    username: {
-      type: String,
-      required: true,
-      index: {
-        global: false
-      }
     }
   },
   { useDocumentTypes: true }
 )
 
-class EventsController {
-  constructor (env, configs) {
-    this.configs = configs
-    this.env = env
+const getModel = function (modelName) {
+  const d = new dynamoose.Dynamoose()
+  d.AWS.config.update({region})
+  const model = d.model(`fusion.${environment}.${modelName}`, defaultSchema)
 
-    const region = (configs.region instanceof Function)
-      ? configs.region(env)
-      : configs.region
+  return {
+    get (id) {
+      return model.get({version, id})
+    },
 
-    this.region = region
+    find () {
+      throw new Error('not implemented')
+    },
 
-    const d = new dynamoose.Dynamoose()
-    d.AWS.config.update({region})
-    this.model = d.model(configs.table || 'maestro.events', configs.schema || defaultSchema)
-  }
+    findOne () {
+      throw new Error('not implemented')
+    },
 
-  get (id) {
-    return this.model.get({env: this.env, id})
-  }
-
-  list (options) {
-    const schema = this.model.$__.schema
-    const filterMap = {}
-    Object.keys(options)
-      .forEach(field => {
-        let operator = 'eq'
-        let value = options[field]
-
-        if (field.endsWith('<')) {
-          field = field.slice(0, -1)
-          operator = 'le'
-        } else if (field.endsWith('>')) {
-          field = field.slice(0, -1)
-          operator = 'ge'
-        // -- `endsWith` is not a valid dynamo search operator
-        // } else if (value.startsWith('*')) {
-        //   value = value.slice(1)
-        //   operator = 'endsWith'
-        } else if (value.endsWith('*')) {
-          value = value.slice(0, -1)
-          operator = 'beginsWith'
-        }
-
-        if (field === 'env') {
-          throw new Error('Cannot specify `env` attribute')
-        }
-
-        // ensure field is part of the schema
-        if (field in schema.attributes) {
-          const whereable = (
-            schema.hashKey.name === field ||
-            schema.rangeKey.name === field ||
-            schema.attributes[field].indexes
-          )
-          const filter = {
-            whereable,
-            field,
-            operator,
-            value: [value]
-          }
-          filterMap[field] = filterMap[field] || {}
-          filterMap[field][operator] = filter
-        // -- ignore stray attributes
-        // } else {
-        //   throw new Error(`${field} is not a schema attribute`)
-        }
+    put (doc) {
+      return new Promise((resolve, reject) => {
+        model.save(Object.assign({}, doc, {version}), (err, data) => (err ? reject(err) : resolve(data)))
       })
-
-    Object.keys(filterMap)
-      .forEach(field => {
-        const filter = filterMap[field]
-        if (filter.ge && filter.le) {
-          filter.between = {
-            whereable: filter.ge.whereable,
-            field: filter.ge.field,
-            operator: 'between',
-            value: [filter.ge.value[0], filter.le.value[0]]
-          }
-          delete filter.ge
-          delete filter.le
-        }
-      })
-
-    const filters = [].concat(...Object.keys(filterMap).map(field =>
-      Object.keys(filterMap[field]).map(operator => filterMap[field][operator])
-    ))
-      .filter(filter => !!filter)
-
-    const where = filters.find(filter => filter.whereable)
-    const queryAction = (filters.length === 0 || where) ? 'query' : 'scan'
-
-    let query = this.model[queryAction]('env').eq(this.env)
-    if (where) {
-      query = query.where(where.field)[where.operator](...where.value)
     }
-    filters
-      .filter(filter => !where || filter.field !== where.field)
-      .forEach(filter => {
-        query = query.filter(filter.field)[filter.operator](...filter.value)
-      })
-
-    return query.exec()
-  }
-
-  put (event) {
-    return new Promise((resolve, reject) => {
-      this.model.create(Object.assign({}, event, {env: this.env}), (err, data) => (err ? reject(err) : resolve(data)))
-    })
-      // don't throw exception on event logging
-      .catch((err) => {
-        console.error(err)
-        return null
-      })
   }
 }
 
-module.exports = EventsController
+module.exports = getModel

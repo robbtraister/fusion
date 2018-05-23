a961857ed5cce2b4e09308e2735ebadc90732148
Robb Traister <robb.traister@gmail.com>
2018-05-23 09:12:54 -0400

    remove aws-sdk dependency from engine-generator

--- engine-generator/zip.sh
+++ engine-generator/zip.sh
@@ -8,6 +8,8 @@
   cd ../engine
   rm -rf node_modules dist
   npm install --production
+  # aws-sdk is still depended on by dynamoose, so manually delete it
+  rm -rf ./node_modules/aws-sdk
   cd -
 
   zip generator.zip -r ../engine ../engine-generator

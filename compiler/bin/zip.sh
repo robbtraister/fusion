097117fa710f20c501d6a54a298c42df5a885c41
Robb Traister <robb.traister@gmail.com>
2018-06-18 09:44:51 -0400

    remove premature optimization in compiler build

--- compiler/bin/zip.sh
+++ compiler/bin/zip.sh
@@ -9,15 +9,9 @@
   cd ../engine
   rm -rf node_modules dist
   npm install --production
-  # aws-sdk is still depended on by dynamoose, so manually delete it
-  rm -rf ./node_modules/aws-sdk
 
   zip ../compiler/dist/engine.zip -r ./
 
   cd ..
   zip compiler/dist/compiler.zip -r compiler engine -x compiler/bin/\* compiler/dist/\*
-
-  # aws-sdk is required by the ./deploy command, so re-install it
-  cd compiler
-  npm install
 )

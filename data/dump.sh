89ce06909ca3287c15ce1781ce22e0aab2548d69
Robb Traister <robb.traister@gmail.com>
2018-06-27 17:02:45 -0400

    use volume/timestamp instead of temp files

--- data/dump.sh
+++ data/dump.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-DUMP_DIR=$(mktemp -d)
-mongodump -o "${DUMP_DIR}" --db "${DB_NAME}" > /dev/null
-TEMP_FILE=$(mktemp)
-TARBALL="${TEMP_FILE}.tar.gz"
-rm -rf "${TEMP_FILE}"
-tar -C "${DUMP_DIR}/${DB_NAME}" -czvf "${TARBALL}" ./ > /dev/null
-rm -rf "${DUMP_DIR}"
-echo "${TARBALL}"
+now=$(date '+%Y-%m-%d-%H-%M-%S')
+(
+  cd /data/dumps
+  DUMP_DIR="./${now}"
+  mkdir -p "${DUMP_DIR}"
+  mongodump -o "${DUMP_DIR}" --db "${DB_NAME}"
+  TARBALL="${DUMP_DIR}.tar.gz"
+  tar -C "${DUMP_DIR}/${DB_NAME}" -czvf "${TARBALL}" ./
+  rm -rf "${DUMP_DIR}"
+)

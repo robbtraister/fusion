443e8d9f65133c537026f0de372664ec2bd306ab
Robb Traister <robb.traister@gmail.com>
2018-07-12 14:42:13 -0400

    use a root-level package.json for a shared version

--- publish.sh
+++ publish.sh
@@ -5,39 +5,27 @@ APP_NAME=${APP_NAME:-'fusion'}
 NOTIFICATIONS_URL=${NOTIFICATIONS_URL:-'http://jenkins-bot.internal.arc.nile.works'}
 SLACK_CHANNEL=${SLACK_CHANNEL:-'fusion-notices'}
 
-TAG='latest'
+VERSION='latest'
 if [ "${RELEASE}" ]
 then
-  TAG='release'
+  VERSION=$(python -c "import json; print json.load(open('./package.json'))['version']")
+  VERSION=${VERSION:-latest}
 fi
-
-version () {
-  if [ "${RELEASE}" ]
-  then
-    name=$1
-    version=$(python -c "import json; print json.load(open('./${name}/package.json'))['version']")
-    echo ${version:-latest}
-  else
-    echo 'latest'
-  fi
-}
 
 buildImage () {
   name=$1
-  v=$(version "$name")
   if [ -f "./${name}/Dockerfile" ]
   then
-    docker build -t "quay.io/washpost/fusion-${name}:${v}" -f "./${name}/Dockerfile" "./${name}"
+    docker build -t "quay.io/washpost/fusion-${name}:${VERSION}" -f "./${name}/Dockerfile" "./${name}"
   else
-    docker build --build-arg "LAMBDA=${name}" -t "quay.io/washpost/fusion-${name}:${v}" -f ./serverless.Dockerfile .
+    docker build --build-arg "LAMBDA=${name}" -t "quay.io/washpost/fusion-${name}:${VERSION}" -f ./serverless.Dockerfile .
   fi
 }
 
 pushImage () {
   name=$1 && \
-  v=$(version "$name") && \
-  docker tag "quay.io/washpost/fusion-${name}:${v}" "quay.io/washpost/fusion-${name}:${v}" && \
-  docker push "quay.io/washpost/fusion-${name}:${v}"
+  docker tag "quay.io/washpost/fusion-${name}:${VERSION}" "quay.io/washpost/fusion-${name}:${VERSION}" && \
+  docker push "quay.io/washpost/fusion-${name}:${VERSION}"
 }
 
 notify () {
@@ -47,14 +35,14 @@ notify () {
 
   if [ "${HAL_TOKEN}" ]
   then
-    curl -X POST -k -H 'Content-Type: application/json' -d "{\"app\":\"${appName:-${APP_NAME}}\",\"cluster\":\"${CLUSTER_NAME}\",\"autoDeploy\":false,\"status\":\"${status}\",\"version\":\"${tag:-${TAG}}\",\"step\":\"${step}\",\"msg\":\"${msg}\"}" ${NOTIFICATIONS_URL}/hubot/notify?token=${HAL_TOKEN}\&room=${SLACK_CHANNEL}\&type=build-status
+    curl -X POST -k -H 'Content-Type: application/json' -d "{\"app\":\"${appName:-${APP_NAME}}\",\"cluster\":\"${CLUSTER_NAME}\",\"autoDeploy\":false,\"status\":\"${status}\",\"version\":\"${tag:-${VERSION}}\",\"step\":\"${step}\",\"msg\":\"${msg}\"}" ${NOTIFICATIONS_URL}/hubot/notify?token=${HAL_TOKEN}\&room=${SLACK_CHANNEL}\&type=build-status
   fi
 }
 
 notifyBuildStart() {
   if [ "${HAL_TOKEN}" ]
   then
-    curl -X POST -k -H 'Content-Type: application/json' -d "{\"app\":\"${appName:-${APP_NAME}}\",\"cluster\":\"${CLUSTER_NAME}\",\"version\":\"${tag:-${TAG}}\"}" ${NOTIFICATIONS_URL}/hubot/notify?token=${HAL_TOKEN}\&room=${SLACK_CHANNEL}\&type=build-started
+    curl -X POST -k -H 'Content-Type: application/json' -d "{\"app\":\"${appName:-${APP_NAME}}\",\"cluster\":\"${CLUSTER_NAME}\",\"version\":\"${tag:-${VERSION}}\"}" ${NOTIFICATIONS_URL}/hubot/notify?token=${HAL_TOKEN}\&room=${SLACK_CHANNEL}\&type=build-started
   fi
 }
 
@@ -65,14 +53,9 @@ notifyBuildError () {
 
 build () {
   name=$1
-  v=$(version "$name")
-
-  (appName="${APP_NAME}-${name}" tag="${v}" notifyBuildStart)
 
   buildImage "${name}" || notifyBuildError "building ${name}"
   pushImage "${name}" || notifyBuildError "pushing ${name}"
-
-  (appName="${APP_NAME}-${name}" tag="${v}" notify 'success' 'build completed')
 }
 
 notifyBuildStart

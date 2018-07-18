ea59c83a267bf4d8c822614de491b8d92c05e182
Robb Traister <robb.traister@gmail.com>
2018-07-18 10:21:27 -0400

    add mock placeholder for fusion:variables so it will compile

--- engine/mock-requires/client.js
+++ engine/mock-requires/client.js
@@ -4,4 +4,10 @@ require('./shared')
 
 const mockRequire = require('mock-require')
 
+// will not be available in client code
 mockRequire('fusion:environment', {})
+
+// will be available in client code
+// webpack will modify to reference the Fusion framework object
+// but compilation will still fail if it cannot resolve to something
+mockRequire('fusion:variables', {})

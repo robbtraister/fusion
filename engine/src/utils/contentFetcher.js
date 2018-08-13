8712ac6b4df10d558fcff971a46eeb1a79e8eaa6
Jonathan Satria <jonathan.satria@washpost.com>
2018-08-13 16:39:29 -0400

    Add timers

--- engine/src/utils/contentFetcher.js
+++ engine/src/utils/contentFetcher.js
@@ -5,6 +5,8 @@ const request = require('request-promise-native')
 const url = require('url')
 
 const debugFetch = require('debug')('fusion:content:sources:fetch')
+const debugTimer = require('debug')('fusion:timer:sources:fetch')
+const timer = require('../timer')
 
 const {
   cacheProxy,
@@ -21,28 +23,30 @@ function pushToCache (cacheKey, cacheValue) {
 }
 
 module.exports = function fetchContent (contentBase, contentUri) {
-  console.log(`fetching from url ${JSON.stringify(contentUri)}`)
-
   const cacheKey = cachePrefix + '_' + contentUri
   const cacheProxyRequest = cacheProxy + `?key=` + cacheKey
   debugFetch(`cache fetching with request  ${JSON.stringify(cacheProxyRequest)}`)
+  let tic = timer.tic()
 
   return request(cacheProxyRequest)
     .then((data) => {
       if (!data) {
         throw Error('data error from cache')
       }
-      console.log(`cache value found for key: ` + cacheKey)
+      debugTimer(`Fetch from cache ${cacheKey}`, tic.toc())
       return data
     })
     .catch(() => {
       debugFetch(`Fetching from source` + url.format(Object.assign(url.parse(contentUri), {auth: null})))
+      tic = timer.tic()
       const contentUrl = url.resolve(contentBase, contentUri)
       const dataPromise = request(contentUrl)
         .then((data) => {
-          debugFetch(`Pushing to cache` + cacheKey)
+          debugTimer(`Fetched from source ${cacheKey}`, tic.toc())
+          tic = timer.tic()
           return pushToCache(cacheKey, data)
             .then(response => {
+              debugTimer(`Pushed to cache`, tic.toc())
               if (response) {
                 return data
               }

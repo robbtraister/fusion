53d72b3e3c612860c5209511bef4eb533f286a59
Robb Traister <robb.traister@gmail.com>
2018-08-20 14:38:43 -0400

    remove jge sources from production

--- engine/src/models/sources/jge.js
+++ engine/src/models/sources/jge.js
@@ -2,12 +2,26 @@
 
 const model = require('../../dao')
 
-module.exports = {
-  find () {
-    return model('jge_config').find()
-  },
+const {
+  isDev
+} = require('../../../environment')
 
-  get (sourceName) {
-    return model('jge_config').get(sourceName)
+module.exports = (isDev)
+  ? {
+    find () {
+      return model('jge_config').find()
+    },
+
+    get (sourceName) {
+      return model('jge_config').get(sourceName)
+    }
   }
-}
+  : {
+    find () {
+      return Promise.resolve([])
+    },
+
+    get (sourceName) {
+      return Promise.resolve(null)
+    }
+  }

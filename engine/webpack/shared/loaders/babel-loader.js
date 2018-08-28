fe49ea8000368693a14796bfef448f67432653df
Robb Traister <robb.traister@gmail.com>
2018-08-28 14:37:42 -0400

    update to babel 7

--- engine/webpack/shared/loaders/babel-loader.js
+++ engine/webpack/shared/loaders/babel-loader.js
@@ -9,16 +9,21 @@ module.exports = {
   options: {
     babelrc: false,
     presets: [
-      'env',
-      'react'
+      '@babel/env',
+      '@babel/react'
     ],
     plugins: [
       ['root-import', {
         rootPathPrefix: '~',
         rootPathSuffix: bundleSrcRoot
       }],
-      'transform-decorators-legacy',
-      'transform-object-rest-spread'
+      ['@babel/proposal-decorators', {
+        legacy: true
+      }],
+      ['@babel/proposal-class-properties', {
+        loose: true
+      }],
+      '@babel/proposal-object-rest-spread'
     ],
     comments: false
   }

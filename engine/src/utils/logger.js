ce3079672284fa96da4226b4c251567e9fcdf708
Todd Mantell <todd.mantell@washpost.com>
2018-09-19 16:22:08 -0500

    tweaked the console output to start with the type of log followed by the json object

--- engine/src/utils/logger.js
+++ engine/src/utils/logger.js
@@ -3,25 +3,25 @@ const { environment, semver, version, functionName } = require('../../environmen
 const LOG_LEVELS = {
   ERROR: 'error',
   INFO: 'info',
-  WARN: 'warning'
+  WARN: 'warn'
 }
 
 const logInformation = (message) => {
   const logObject = getLogObject(LOG_LEVELS.INFO, message)
 
-  console.info(JSON.stringify(logObject))
+  console.info(`${LOG_LEVELS.INFO}: ${JSON.stringify(logObject)}`)
 }
 
 const logError = (message) => {
   const logObject = getLogObject(LOG_LEVELS.ERROR, message)
 
-  console.error(JSON.stringify(logObject))
+  console.error(`${LOG_LEVELS.ERROR}: ${JSON.stringify(logObject)}`)
 }
 
 const logWarning = (message) => {
   const logObject = getLogObject(LOG_LEVELS.WARN, message)
 
-  console.warn(JSON.stringify(logObject))
+  console.warn(`${LOG_LEVELS.WARN}: ${JSON.stringify(logObject)}`)
 }
 
 function getLogObject (logLevel, message) {
@@ -31,9 +31,9 @@ function getLogObject (logLevel, message) {
     logLevel,
     message,
     logType: 'page rendering time',
-    semver,
+    fusionVersion: semver,
     values: { 'feature 1': '100ms' },
-    version
+    lambdaDeployment: version
   }
 }
 

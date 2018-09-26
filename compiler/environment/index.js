62a5c7d03f3e0fad2023f66f67714b8fc5b56123
Robb Traister <robb.traister@gmail.com>
2018-09-26 08:37:05 -0400

    fix region env var

--- compiler/environment/index.js
+++ compiler/environment/index.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   environment: process.env.ENVIRONMENT,
-  region: process.env.REGION,
+  region: process.env.AWS_REGION,
   S3BucketVersioned: process.env.S3BUCKET_VERSIONED,
   S3BucketDiscrete: process.env.S3BUCKET_DISCRETE
 }

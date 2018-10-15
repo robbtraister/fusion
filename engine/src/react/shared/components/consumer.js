a11e7310f7eea80d711f248851618a31f43ff1a8
sb-rlje <Skylar.Banks@washpost.com>
2018-10-15 11:08:43 -0400

    Split contentEditable into editableField and editableContent

--- engine/src/react/shared/components/consumer.js
+++ engine/src/react/shared/components/consumer.js
@@ -20,11 +20,21 @@ const getContextProps = (props, context) => {
 const createContextElement = (Component, props, context) => {
   const { props: contextProps, children } = getContextProps(props, context)
 
-  contextProps.contentEditable = (isClient && Fusion.isAdmin)
-    ? (prop) => ({
-      'data-content-editable': prop,
+  contextProps.editableField= (isClient && Fusion.isAdmin)
+    ? (contentField) => ({
       'data-feature': props.id,
-      'contenteditable': 'true'
+      'data-field-editable': contentField,
+      'contentEditable': 'true'
+    })
+    : () => ({})
+
+  contextProps.editableContent = (isClient && Fusion.isAdmin)
+    ? (contentField, contentId, elementId) => ({
+      'data-feature': props.id,
+      'data-content-editable': contentField,
+      'data-content-id': contentId,
+      'data-element-id': elementId,
+      'contentEditable': 'true'
     })
     : () => ({})
 

1d39a45639c63d12638e425df143a53e07eba5ae
Robb Traister <robb.traister@gmail.com>
2018-10-31 17:04:44 -0400

    don't log a stack trace on 4xx errors

--- engine/src/errors/handlers.js
+++ engine/src/errors/handlers.js
@@ -9,19 +9,25 @@ const {
 module.exports = [
   (err, req, res, next) => {
     if (err instanceof RedirectError) {
-      res.redirect(err.statusCode, err.location)
-    } else {
-      next(err)
+      return res.redirect(err.statusCode, err.location)
     }
+
+    err.statusCode = err.statusCode || 500
+    // don't log an error stack for 4xx errors
+    if (err.statusCode >= 500) {
+      console.error(err)
+    } else if (err.message) {
+      console.log(err.message)
+    }
+
+    next(err)
   },
 
   (isDev)
     ? (err, req, res, next) => {
-      console.error(err)
-      res.status(err.statusCode || 500).send(err.message || err)
+      res.status(err.statusCode).send(err.message || err)
     }
     : (err, req, res, next) => {
-      console.error(err)
-      res.sendStatus(err.statusCode || 500)
+      res.sendStatus(err.statusCode)
     }
 ]

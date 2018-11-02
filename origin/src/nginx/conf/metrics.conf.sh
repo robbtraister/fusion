dd9d5b4ee00543835c5e9354ec8682c56ff4211e
Jonathan Satria <jonathan.satria@washpost.com>
2018-11-02 13:56:21 -0400

    Add environment tag, fix arcSite tag naming

--- origin/src/nginx/conf/metrics.conf.sh
+++ origin/src/nginx/conf/metrics.conf.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 cat <<EOB
-    statsd_timing             "arc.fusion.origin.latency#nile_env:${NILE_ENV},arc-site:\${arcSite}" "\$request_time";
+    statsd_timing             "arc.fusion.origin.latency#nile_env:${NILE_ENV},environment:\${environment},arcSite:\${arcSite}" "\$request_time";
 EOB
-

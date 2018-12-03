69a0a52d2415a14158d6c85cd4860bfb80f32c63
Robb Traister <robb.traister@gmail.com>
2018-12-03 11:39:49 -0500

    simplify substitute implementation; require non-grouping syntax for prefixes

--- engine/src/engines/_shared/substitute.js
+++ engine/src/engines/_shared/substitute.js
@@ -6,11 +6,7 @@ module.exports = (obj, content = {}, prefix = '') => {
   const regexp = new RegExp(`\\{\\{${prefix ? prefix.replace(/\.*$/, '.') : ''}([^}]+)\\}\\}`, 'g')
   return JSON.parse(
     JSON.stringify(obj)
-      .replace(regexp, function () {
-        const original = arguments[0]
-        // use the last group (we don't know what's in prefix)
-        // but the arguments array ends with offset and the entire string
-        const property = arguments[arguments.length - 3]
+      .replace(regexp, function (original, property) {
         const replacement = _get(content, property) || original
         return JSON.stringify(replacement).replace(/^"(.*)"$/, (_, value) => value)
       })

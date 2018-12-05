c3b62f0a58bf979b6302368d84c8845152e1f397
Robb Traister <robb.traister@gmail.com>
2018-12-05 11:33:04 -0500

    update nginx metrics statement

--- origin/src/nginx/conf/metrics.conf.sh
+++ origin/src/nginx/conf/metrics.conf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cat <<EOB
-    statsd_timing             "arc.fusion.origin.latency#nile_env:${NILE_ENV},environment:\${environment},arcSite:\${arcSite}" "\$request_time";
+    statsd_timing             "arc.fusion.origin.latency#nile_env:${NILE_ENV},environment:\${environment},arc_site:\${arcSite},status_code:\${status}" "\$request_time";
 EOB

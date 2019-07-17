21a66aa36866a27e08ef28fe82f4356cd51461c5
Robb Traister <ltraister@virtru.com>
2019-07-17 08:43:58 -0400

    move descendants logic to fusion-components

--- src/utils/descendants.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-function getDescendants (node) {
-  const children = [].concat((node && node.children) || [])
-  return children.concat(...children.map(getDescendants))
-}
-
-module.exports = getDescendants
